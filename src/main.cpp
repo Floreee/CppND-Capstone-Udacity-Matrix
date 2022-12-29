c312c0f76294d38afe838a27dacdaf522a4d072a
Floreee <f.buerchner@gmail.com>
2022-12-29 20:36:18 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <dirent.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <cstring>
 #include <algorithm>
 #include <filesystem>
@@ -14,183 +11,135 @@
 
 int main(int argc, char *argv[])
 {
-    // Create an instance of the ImageBMP class
-    Udacity::ImageBMP image;
+    if (argc < 1)
+    {
+        std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g]" << std::endl;
+        std::cout << "Options:" << std::endl;
+        std::cout << "   -i INPUT_FILE  : input file name (must be 24-bit BMP)" << std::endl;
+        std::cout << "   -o OUTPUT_FILE : output file name" << std::endl;
+        std::cout << "   -fh            : flip image horizontally" << std::endl;
+        std::cout << "   -fv            : flip image vertically" << std::endl;
+        std::cout << "   -g             : convert image to grayscale" << std::endl;
+        std::cout << "   -h             : print this help text and exit" << std::endl;
+        std::cout << "   -t            : test matrix class" << std::endl;
+        return 0;
+    }
 
-    // Parse the command line arguments
-    bool flip_horizontal = false;
-    bool flip_vertical = false;
+    // Parse command line arguments
+    std::filesystem::path inputPath;
+    std::filesystem::path outputPath;
+    bool flipHorizontal = false;
+    bool flipVertical = false;
     bool grayscale = false;
-    bool operation = false;
-    std::string input_path = "";
-    std::string output_path = "";
-    std::string input_file = "";
-    std::string output_file = "";
-    for (int i = 1; i < argc; i++)
+
+    for (int i = 1; i < argc; ++i)
     {
         std::string arg = argv[i];
-
-        // Input file or folder
         if (arg == "-i")
         {
-            if (i + 1 < argc)
-            {
-                input_path = argv[i + 1];
-                input_path = std::filesystem::absolute(input_path);
-                if (!std::filesystem::exists(input_path))
-                {
-                    std::cout << "Error: input file/folder does not exist." << std::endl;
-                    return 1;
-                }
-            }
-            else
-            {
-                std::cerr << "Error: missing input file or folder name" << std::endl;
-                return 1;
-            }
+            inputPath = argv[++i];
         }
-
-        // Output file
-        if (arg == "-o")
+        else if (arg == "-o")
         {
-            if (i + 1 < argc)
-            {
-                output_path = argv[i + 1];
-                output_path = std::filesystem::absolute(output_path);
-            }
-            else
-            {
-                std::cerr << "Error: missing output file name" << std::endl;
-                return 1;
-            }
+            outputPath = argv[++i];
         }
-
-        // Flip horizontally
-        if (arg == "-fh")
+        else if (arg == "-fh")
         {
-            flip_horizontal = true;
+            flipHorizontal = true;
         }
-
-        // Flip vertically
-        if (arg == "-fv")
+        else if (arg == "-fv")
         {
-            flip_vertical = true;
+            flipVertical = true;
         }
-
-        // Grayscale
-        if (arg == "-g")
+        else if (arg == "-g")
         {
             grayscale = true;
         }
-
-        // Test 
-        if (arg == "-t")
+        else if (arg == "-t")
         {
             test();
+        }
+        else if (arg == "-h")
+        {
+            std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g] [-h] [-t]" << std::endl;
+            std::cout << "   -i INPUT_FILE  : input file name (must be 24-bit BMP)" << std::endl;
+            std::cout << "   -o OUTPUT_FILE : output file name" << std::endl;
+            std::cout << "   -fh            : flip image horizontally" << std::endl;
+            std::cout << "   -fv            : flip image vertically" << std::endl;
+            std::cout << "   -g             : convert image to grayscale" << std::endl;
+            std::cout << "   -h             : print this help text and exit" << std::endl;
+            std::cout << "   -t             : test matrix class" << std::endl;
             return 0;
-        }
-
-        // Help
-        if (arg == "-h")
-        {
-            std::cout << "Usage: program -i input_file_or_folder -o output_file -fh -fv -g -op" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "  -i input_file_or_folder  Specify the input file or folder (only .bmp files are supported)" << std::endl;
-            std::cout << "  -o output_file           Specify the output file" << std::endl;
-            std::cout << "  -fh                      Flip the image horizontally" << std::endl;
-            std::cout << "  -fv                      Flip the image vertically" << std::endl;
-            std::cout << "  -g                       Convert the image to grayscale" << std::endl;
-            std::cout << "  -t                       Test matrix class and its functions (use this argument solely)" << std::endl;
-            return 0;
-        }
-    }
-
-    // Check if the input path is a folder
-    DIR *dir = opendir(input_path.c_str());
-    if (dir)
-    {
-        // Input path is a folder, so process all .bmp files in the folder
-        std::vector<std::string> files;
-        struct dirent *ent;
-        while ((ent = readdir(dir)) != NULL)
-        {
-            if (ent->d_type == DT_REG) // Regular file
-            {
-                std::string file_name = ent->d_name;
-                std::string file_ext = file_name.substr(file_name.find_last_of(".") + 1);
-                std::transform(file_ext.begin(), file_ext.end(), file_ext.begin(), ::tolower);
-                if (file_ext == "bmp")
-                {
-                    files.push_back(file_name);
-                }
-            }
-        }
-        closedir(dir);
-
-        // Process the .bmp files in the folder
-        for (const std::string &file : files)
-        {
-            std::string input_file = input_path + '/' + file;
-            if (output_path == "")
-            {
-                output_path = input_path;
-            }
-            std::string output_file = output_path + "/edit_" + input_file; 
-
-            image.input(input_file);
-            if (flip_horizontal)
-            {
-                image.flip(true);
-            }
-            if (flip_vertical)
-            {
-                image.flip(false);
-            }
-            if (grayscale)
-            {
-                image.grayscale();
-            }
-            image.output(output_file);
-        }
-    }
-    else
-    {
-        // Input path is a single file, so process it if it is a .bmp file
-        std::string file_name = input_path.substr(input_path.find_last_of("/") + 1);
-        std::string file_ext = file_name.substr(file_name.find_last_of(".") + 1);
-        std::transform(file_ext.begin(), file_ext.end(), file_ext.begin(), ::tolower);
-        if (file_ext == "bmp")
-        {
-            image.input(input_path);
-            if (flip_horizontal)
-            {
-                image.flip(true);
-            }
-            if (flip_vertical)
-            {
-                image.flip(false);
-            }
-            if (grayscale)
-            {
-                image.grayscale();
-            }
-            if (output_path == "")
-            {
-                output_path = input_path.substr(0, input_path.find_last_of("/"));
-                output_path += "/edit_" + file_name;
-            }
-            else if  ((output_path.find(".bmp") == std::string::npos))
-            {
-                output_path = std::filesystem::absolute(output_path).string() + "/edit_" + file_name;
-            }
-            image.output(output_path);
         }
         else
         {
-            std::cerr << "Error: invalid input file (only .bmp files are supported)" << std::endl;
+            std::cerr << "Invalid argument: " << arg << std::endl;
             return 1;
         }
     }
+
+    // Check that required arguments are provided
+    if (inputPath.empty())
+    {
+        std::cerr << "Missing required argument(s) for input" << std::endl;
+        std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g] [-h] [-t]" << std::endl;
+        return 1;
+    }
+    
+    // Make path absolute 
+    inputPath = std::filesystem::absolute(inputPath);
+    
+    if (outputPath.empty())
+    {
+        std::cerr << "Missing required argument(s) for output" << std::endl;
+        std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g] [-h] [-t]" << std::endl;
+        return 1;
+        
+    }
+
+    // Make path absolute 
+    outputPath = std::filesystem::absolute(outputPath); 
+
+    if (!std::filesystem::exists(inputPath)) 
+    {
+        std::cerr << "Input path does not exist: " << inputPath.string() << std::endl;
+        std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g] [-h] [-t]" << std::endl;
+        return 1;
+    }
+    else if (!std::filesystem::exists(outputPath.parent_path()))
+    {
+        std::cerr << "Output path does not exist: " << outputPath.string() << std::endl;
+        std::cerr << "Usage: UdacityMatrix -i INPUT_FILE -o OUTPUT_FILE [-fh] [-fv] [-g] [-h] [-t]" << std::endl;
+        return 1;
+    }
+
+    // Create an ImageBMP object
+    Udacity::ImageBMP image;
+
+    // Load the image from the input file
+    std::cout << "Reading file: " << inputPath << std::endl;
+    image.input(inputPath.string());
+
+    // Perform requested operations on the image
+    if (flipHorizontal)
+    {
+        std::cout << "..horizontal flip" << std::endl;
+        image.flip(true);
+    }
+    if (flipVertical)
+    {
+        std::cout << "..vertical flip" << std::endl;
+        image.flip(false);
+    }
+    if (grayscale)
+    {
+        std::cout << "..grayscale" << std::endl;
+        image.grayscale();
+    }
+
+    // Save the image to the output file
+    std::cout << "Writing file: " << outputPath<< std::endl;
+    image.output(outputPath.string());
 
     return 0;
 }
